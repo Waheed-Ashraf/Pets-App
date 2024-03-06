4b199b9382b713129f521c6d708b7224f946a277
waheed_ashrf <washraf124@gmail.com>
2024-03-06 03:03:55 +0200

    Animated Search Bar

--- lib/Features/Dogs/presentation/views/dogs_view.dart
+++ lib/Features/Dogs/presentation/views/dogs_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pets_app/Core/widgets/custom_search_textfield.dart';
 
 class DogsView extends StatelessWidget {
   const DogsView({super.key});
@@ -6,9 +7,26 @@ class DogsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('HomeView'),
-      ),
+      body: SafeArea(child: DogsViewBody()),
+    );
+  }
+}
+
+class DogsViewBody extends StatelessWidget {
+  const DogsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          width: double.infinity,
+        ),
+        CustomSearchTextField(
+          onTap: () {},
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pets_app/Core/theme/app_theme.dart';
-import 'package:pets_app/Features/AuthFeature/presentation/views/signing_view/signing_view.dart';
 import 'package:pets_app/Features/BottomNavBar/bottom_nav_bar.dart';
 import 'package:pets_app/firebase_options.dart';
 
@@ -21,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightMood,
-      home: const SigningView(),
+      home: const BottomBarScreen(),
     );
   }
 }
