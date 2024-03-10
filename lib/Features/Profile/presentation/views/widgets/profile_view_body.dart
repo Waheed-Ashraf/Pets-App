82e7521fc1f666ddf70e02f03160de0e465e2815
mohamed3sman <mohamedabdoosman12@gmail.com>
2024-03-10 23:40:38 +0200

    viewing user name

--- lib/Features/Profile/presentation/views/widgets/profile_view_body.dart
+++ lib/Features/Profile/presentation/views/widgets/profile_view_body.dart
@@ -1,10 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pets_app/core/utils/app_styles.dart';
 import 'curve_and_image.dart';
 import 'custom_list_tile.dart';
 
-class ProfileViewBody extends StatelessWidget {
+class ProfileViewBody extends StatefulWidget {
   const ProfileViewBody({super.key});
+
+  @override
+  State<ProfileViewBody> createState() => _ProfileViewBodyState();
+}
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+User? _user;
+String? userName;
+
+@override
+class _ProfileViewBodyState extends State<ProfileViewBody> {
+  void _getCurrentUser() {
+    User? currentUser = _auth.currentUser;
+    setState(() {
+      _user = currentUser;
+    });
+  }
+
+  void _getUserData() async {
+    print('${_user!.email}');
+    QuerySnapshot querySnapshot = await _firestore
+        .collection('users')
+        .where('email', isEqualTo: _user!.email)
+        .get();
+
+    // Retrieve the username from the document
+    if (querySnapshot.docs.isNotEmpty) {
+      setState(() {
+        userName = (querySnapshot.docs[0].data()
+                as Map<String, dynamic>)['username'] ??
+            '...';
+        profileImage =
+            (querySnapshot.docs[0].data() as Map<String, dynamic>)['userimage'];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentUser();
+    _getUserData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +64,7 @@ class ProfileViewBody extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(top: 40, bottom: 20),
                 child: Text(
-                  'Mohamed Osman',
+                  '$userName',
                   style: AppStyles.styleMedium20,
                 ),
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:pets_app/Core/theme/app_theme.dart';
 import 'package:pets_app/Core/utils/service_locator.dart';
 import 'package:pets_app/Features/AuthFeature/presentation/views/signing_view/signing_view.dart';
-import 'package:pets_app/Features/BottomNavBar/bottom_nav_bar.dart';
-import 'package:pets_app/Features/Cats/presentation/views/cats_view.dart';
 import 'package:pets_app/firebase_options.dart';
 
 void main() async {
@@ -27,7 +25,7 @@ class MyApp extends StatelessWidget {
       // locale: DevicePreview.locale(context),
       // builder: DevicePreview.appBuilder,
       theme: lightMood,
-      home: const BottomBarScreen(),
+      home: const SigningView(),
     );
   }
 }
