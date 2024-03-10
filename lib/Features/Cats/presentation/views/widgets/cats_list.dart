f99130cb0c0de764778133f31d63e1827e611119
waheed_ashrf <washraf124@gmail.com>
2024-03-10 05:25:47 +0200

    adding animation to Cats list

--- lib/Features/Cats/presentation/views/widgets/cats_list.dart
+++ lib/Features/Cats/presentation/views/widgets/cats_list.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,37 +16,39 @@ class CatsList extends StatelessWidget {
     return BlocBuilder<CatsBreedsCubit, CatsBreedsState>(
       builder: (context, state) {
         if (state is CatsBreedsLoaded) {
-          return MasonryGridView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: 50,
-            gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: crossAxisCount),
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: (state.catsBreedsList[index].image) == null
-                        ? Container()
-                        : CachedNetworkImage(
-                            imageUrl: state.catsBreedsList[index].image!.url,
-                            fit: BoxFit.cover,
-                            // placeholder: (context, url) => Shimmer.fromColors(
-                            //   baseColor: Colors.grey[850]!,
-                            //   highlightColor: Colors.grey[800]!,
-                            //   child: Container(
-                            //     height: state.catsBreedsList[index].image!.height,
+          return FadeInUp(
+            child: MasonryGridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: 50,
+              gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: crossAxisCount),
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: (state.catsBreedsList[index].image) == null
+                          ? Container()
+                          : CachedNetworkImage(
+                              imageUrl: state.catsBreedsList[index].image!.url,
+                              fit: BoxFit.cover,
+                              // placeholder: (context, url) => Shimmer.fromColors(
+                              //   baseColor: Colors.grey[850]!,
+                              //   highlightColor: Colors.grey[800]!,
+                              //   child: Container(
+                              //     height: state.catsBreedsList[index].image!.height,
 
-                            //     decoration: BoxDecoration(
-                            //       color: Colors.black,
-                            //       borderRadius: BorderRadius.circular(16.0),
-                            //     ),
-                            //   ),
-                            // ),
-                          )),
-              );
-            },
+                              //     decoration: BoxDecoration(
+                              //       color: Colors.black,
+                              //       borderRadius: BorderRadius.circular(16.0),
+                              //     ),
+                              //   ),
+                              // ),
+                            )),
+                );
+              },
+            ),
           );
         } else if (state is CatsBreedsFailuer) {
           return CustomErrorWidget(errMessage: state.errorMessage);
