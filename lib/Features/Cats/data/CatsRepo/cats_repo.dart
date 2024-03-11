b9cb5278e725542bc5e5bda7d3885810d6710c09
waheed_ashrf <washraf124@gmail.com>
2024-03-11 17:21:52 +0200

    search repo done

--- lib/Features/Cats/data/CatsRepo/cats_repo.dart
+++ lib/Features/Cats/data/CatsRepo/cats_repo.dart
@@ -1,7 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:pets_app/Core/errors/Failuer.dart';
 import 'package:pets_app/Features/Cats/data/Models/CatsModels/cats_model.dart';
+import 'package:pets_app/Features/Cats/data/Models/SearchCatsModels/search_cats_model.dart';
 
 abstract class CatsRepo {
   Future<Either<Failure, List<CatModel>>> fetchCatsBreedsList();
+  Future<Either<Failure, List<SearchCatBreedModel>>>
+      fetchSearchedCatsBreedsList({required String breedsId});
 }
--- lib/Features/Cats/data/CatsRepo/cats_repo_imp.dart
+++ lib/Features/Cats/data/CatsRepo/cats_repo_imp.dart
@@ -5,6 +5,7 @@ import 'package:pets_app/Core/network/api_constance.dart';
 import 'package:pets_app/Core/utils/api_service.dart';
 import 'package:pets_app/Features/Cats/data/Models/CatsModels/cats_model.dart';
 import 'package:pets_app/Features/Cats/data/CatsRepo/cats_repo.dart';
+import 'package:pets_app/Features/Cats/data/Models/SearchCatsModels/search_cats_model.dart';
 
 class CatsRepoImp implements CatsRepo {
   final ApiService _apiService;
@@ -17,6 +18,26 @@ class CatsRepoImp implements CatsRepo {
       List<CatModel> itemData = [];
       for (var element in data) {
         itemData.add(CatModel.fromJson(element));
+      }
+      return right(itemData);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<SearchCatBreedModel>>>
+      fetchSearchedCatsBreedsList({required String breedsId}) async {
+    try {
+      var data = await _apiService.get(
+          endPoint: ApiConstance.searchByCatBreedsId(breedsId));
+      List<SearchCatBreedModel> itemData = [];
+      for (var element in data) {
+        itemData.add(SearchCatBreedModel.fromJson(element));
       }
       return right(itemData);
     } catch (e) {
